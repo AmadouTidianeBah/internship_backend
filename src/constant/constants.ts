cf172777ce2f129afceeb97934c29fa886dad8d1
AmadouTidianeBah <amadoudianebah@gmail.com>
2024-09-03 20:14:14 +0000

    Project init

---
+++ src/constant/constants.ts
@@ -0,0 +1,11 @@
+export enum Role {
+    STUDENT = 'STUDENT',
+    COMPANY = 'COMPANY',
+    ADMIN = 'ADMIN'
+}
+
+export enum Status {
+    PENDING = 'PENDING',
+    ACCEPTED = 'ACCEPTED',
+    REJECTED = 'REJECTED'
+}
